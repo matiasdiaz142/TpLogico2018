2b4b76afcd2b0d4fd472ea53d25e64c9606c957c
Matias Diaz <matiasdiaz142@gmail.com>
2018-08-06 00:39:04 -0300

    Update TpLogico(1eraEntrega).pl

--- TpLogico(1eraEntrega).pl
+++ TpLogico(1eraEntrega).pl
@@ -14,13 +14,11 @@ mira(maiu,starWars).
 mira(maiu,onePiece).
 mira(maiu,got).
 
+%Cosas que mira Pedro
+mira(pedro,got).
+
 %Cosas que mira Gaston.
 mira(gaston,hoc).
-
-%Series populares.
-popular(got).
-popular(hoc).
-popular(starWars).
 
 %Series que planeanVer
 planeaVer(juan,hoc).
@@ -45,6 +43,10 @@ paso(starWars, 3, 2, relacion(parentesco, vader, luke)).
 paso(himym, 1, 1, relacion(amorosa, ted, robin)).
 paso(himym, 4, 3, relacion(amorosa, swarley, robin)).
 paso(got, 4, 5, relacion(amistad, tyrion, dragon)).
+paso(got, 3, 2, plotTwist([suenio,sinPiernas])).
+paso(got, 3, 12, plotTwist([fuego,boda])).
+paso(supercampeones, 9, 9, plotTwist([suenio,coma,sinPiernas])).
+paso(drHouse, 8, 7, plotTwist([coma,pastillas])).
 
 %leDijo/4
 leDijo(gaston, maiu, got, relacion(amistad, tyrion, dragon)).
@@ -53,6 +55,9 @@ leDijo(nico, juan, got, muerte(tyrion)).
 leDijo(aye, juan, got, relacion(amistad, tyrion, john)).
 leDijo(aye, maiu, got, relacion(amistad, tyrion, john)).
 leDijo(aye, gaston, got, relacion(amistad, tyrion, dragon)).
+leDijo(nico,juan,futurama,muerte(seymourDiera)).
+leDijo(pedro,aye,got,relacion(amistad,tyrion,dragon)).
+leDijo(pedro,nico,got,relacion(parentesco,tyrion,dragon)).
 
 %Punto 3
 %esSpoiler(Serie,Spoiler)
@@ -60,7 +65,7 @@ esSpoiler(Serie,Spoiler):-paso(Serie,_,_,Spoiler).
 
 %Punto 4
 %leSpoileo(Persona1,Persona2,Serie)
-leSpoileo(Persona1,Persona2,Serie):- fanaticoSerie(Persona2,Serie), paso(Serie,_,_,Spoiler), leDijo(Persona1,Persona2,Serie,Spoiler).
+leSpoileo(Persona1,Persona2,Serie):- fanaticoSerie(Persona2,Serie),leDijo(Persona1,Persona2,Serie,Spoiler),esSpoiler(Serie,Spoiler).
 
 %Punto 5
 %televidenteResponsable(Persona)
@@ -68,62 +73,14 @@ televidenteResponsable(Persona):- fanaticoSerie(Persona,_),not(leSpoileo(Persona
 
 %Punto 6
 vieneZafando(Persona,Serie):- fanaticoSerie(Persona,Serie),not(leSpoileo(_,Persona,Serie)),popular(Serie).
-vieneZafando(Persona,Serie):- fanaticoSerie(Persona,Serie),not(leSpoileo(_,Persona,Serie)),
-forall(paso(Serie,Temporada,_,_), pasoAlgoFuerte(Serie,Temporada)).
+vieneZafando(Persona,Serie):- fanaticoSerie(Persona,Serie),not(leSpoileo(_,Persona,Serie)),paso(Serie,_,_,_),
+forall(paso(Serie,Temporada,_,_), pasoAlgoFuerte(Serie,Temporada,_)).
 %La variable que entraria sin ligar seria la Temporada, asi verifica que en todas sus temporadas hayan pasado cosas fuertes.
 
-pasoAlgoFuerte(Serie,Temporada):- paso(Serie,Temporada,_,muerte(_)).
-pasoAlgoFuerte(Serie,Temporada):- paso(Serie,Temporada,_,relacion(amorosa,_,_)).
-pasoAlgoFuerte(Serie,Temporada):- paso(Serie,Temporada,_,relacion(parentesco,_,_)).
-
+pasoAlgoFuerte(Serie,Temporada,muerte(Persona)):- paso(Serie,Temporada,_,muerte(Persona)).
+pasoAlgoFuerte(Serie,Temporada,relacion(amorosa,P1,P2)):- paso(Serie,Temporada,_,relacion(amorosa,P1,P2)).
+pasoAlgoFuerte(Serie,Temporada,relacion(parentesco,P1,P2)):- paso(Serie,Temporada,_,relacion(parentesco,P1,P2)).
 
 %Definimos fanatico de una serie como a alguien que mira o planea ver una serie
 fanaticoSerie(Persona,Serie):-mira(Persona,Serie).
 fanaticoSerie(Persona,Serie):-planeaVer(Persona,Serie).
-
-%run_tests.
-:- begin_tests(spoileres).
-
-test(es_spoiler_emperor,nondet) :-
-	esSpoiler(starWars,muerte(emperor)).
-  
-test(no_es_spoiler_pedro,fail) :-
-	esSpoiler(starWars,muerte(pedro)).
-
-test(hay_algun_spoiler,nondet) :-
-	esSpoiler(starWars,muerte(_)).
-    
-test(es_spoiler_relacion,nondet) :-	
-	esSpoiler(starWars,relacion(parentesco,anakin,rey)).
-	
-test(no_es_spoiler_relacion,fail) :-	
-	esSpoiler(starWars,relacion(padre,anakin,lavezzi)).
-	
-test(hay_algun_spoiler_relacion,nondet) :-	
-	esSpoiler(starWars,relacion(parentesco,_,_)).
-	
-test(le_spoileo,nondet) :-	
-	leSpoileo(gaston,maiu,got).
-	
-test(le_spoileo2,nondet) :-	
-	leSpoileo(nico,maiu,starWars).
-	
-test(televidente_responsable,set(Personas == [juan,aye,maiu])) :-	
-	televidenteResponsable(Personas).
-	
-test(no_televidente_responsable,set(Personas == [nico,gaston])) :-	
-	fanaticoSerie(Personas,_),not(televidenteResponsable(Personas)).
-
-test(hay_algun_televidente_responsable,nondet) :-	
-	televidenteResponsable(_).
-		
-test(viene_zafando_maiu) :-	
-	not(vieneZafando(maiu,_)).
-
-test(viene_zafando_juan,set(Series == [himym,got,hoc])) :-
-	vieneZafando(juan,Series).
-
-test(viene_zafando_nico,[true(Persona == nico), nondet]) :-
-	vieneZafando(Persona,starWars).
-	
-:- end_tests(spoileres).
